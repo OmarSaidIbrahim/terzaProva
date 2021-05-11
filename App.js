d30b25029ad1696b9945e3a3905dcba4cf7c2895
Omar <omarsaidita@gmail.com>
2021-05-11 13:17:47 +0100

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ export default class App extends Component {
   render() {
     return (
       <View style={{flex:1, backgroundColor: "#253DF9"}}>
-        <Text style={{color: "white", fontSize: 20}}> textInComponent 2 </Text>
         {/* <LottieView source={require('./Lottie/33756-community-logo-animation.json')} autoPlay loop={false} /> */}
         <LottieView source={require('./Lottie/ezgif.com-crop.mp4.lottie.json')} autoPlay loop={false} />
         
